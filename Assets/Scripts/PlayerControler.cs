1c609a53c1da80bc6d845804075a3d9adb8ece1a
Tikky-Dev <44531270+Tikky-Dev@users.noreply.github.com>
2020-09-10 18:21:19 +0200

    tales of blue - start of a project

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+
+    /* Global variables */
+
+    public float speed = 5f;
+
+    /* Global variables */
+
+    /* Update metod */
+    public void Update()
+    {
+        
+    }
+
+
+    /* Movemant function */
+    public void Move() {
+        
+    }
+}
